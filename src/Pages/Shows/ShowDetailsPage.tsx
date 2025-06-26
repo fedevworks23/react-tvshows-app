c06ee27f77f71ed0ae1aa775b7b07fd3f8c4c792
fedevworks23 <fedevworks@gmail.com>
2025-06-26 17:18:28 +0530

    File name change for show navbar

--- src/Pages/Shows/ShowDetailsPage.tsx
+++ src/Pages/Shows/ShowDetailsPage.tsx
@@ -1,10 +1,10 @@
 import { Outlet } from "react-router";
-import ShowsNavbar from "./ShowsNavbar";
+import ShowDetailsNavbar from "./ShowDetailsNavbar";
 
 const ShowDetail = () => {
   return (
     <div className="bg-white px-2 md:px-8 py-8">
-      <ShowsNavbar />
+      <ShowDetailsNavbar />
 
       <div className="fade-in-animation">
         <Outlet />
