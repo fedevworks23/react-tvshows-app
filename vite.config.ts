28042a4203c635dc893d27b265d1cf32b1ee5cd9
fedevworks23 <fedevworks@gmail.com>
2025-08-08 15:49:05 +0530

    gh-pages setup

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,21 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/react-tvshows-app/',
+  // server: {
+  //   port: 3000,
+  //   open: true,
+  //   host: '' // Set to true to allow access from other devices on the network
+  // },
+  // build: {
+  //   outDir: 'build',
+  //   sourcemap: true,
+  //   rollupOptions: {
+  //     output: {
+  //       entryFileNames: 'assets/[name].js',
+  //       chunkFileNames: 'assets/[name].js',
+  //       assetFileNames: 'assets/[name].[ext]'
+  //     }
+  //   }
+  // },
 })
